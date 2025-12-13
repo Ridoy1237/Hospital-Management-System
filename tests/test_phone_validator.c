655591046bcd1d27291d982f32d5db8f4ec28c66
fahad <fahad@hms.com>
2025-12-13 20:01:59 +0600

    add test case

--- tests/test_phone_validator.c
+++ tests/test_phone_validator.c
@@ -21,11 +21,30 @@ int test_validate_invalidphone() {
 
 }
 
+int test_validate_validphone_by_fahad() {
+    char phone[]="01589898989";
+    int expected=1;
+    int actual=utils_is_valid_phone(phone);
+    int succes= actual==expected;
+    printf("\n\nValid phone number: \n");
+    printf("actual: %d, expected: %d,succes:%d\n",actual,expected,actual==expected);
 
+}
 
+int test_validate_invalidphone_by_fahad() {
+    char phone[]="0123456";
+    int expected=0;
+    int actual=utils_is_valid_phone(phone);
+    int succes= actual==expected;
+    printf("\nInvalid phone number: \n");
+    printf("actual: %d, expected: %d,succes:%d\n",actual,expected,actual==expected);
+
+}
 
 int main()
 {
     test_validate_validphone();
     test_validate_invalidphone();
+    test_validate_validphone_by_fahad();
+    test_validate_invalidphone_by_fahad();
 }
